4fa1bab6360fdc121c8d58a1bdce120114955bb9
alexey-gm <gaidaialexey0502@gmail.com>
2024-09-23 22:40:11 +0700

    add photos, and collect data in obj

---
+++ app/src/main/java/com/example/artspace/Data.kt
@@ -0,0 +1,36 @@
+package com.example.artspace
+
+object Data {
+    val photos = listOf(
+        Photo(
+            id = R.drawable.alyonushka,
+            title = "Аленушка",
+            author = "Виктор Васнецов",
+            year = 1881
+        ),
+        Photo(
+            id = R.drawable.bears_in_the_forest,
+            title = "Мишки в лесу",
+            author = "Иван Шишкин",
+            year = 1889
+        ),
+        Photo(
+            id = R.drawable.heroes,
+            title = "Богатыри",
+            author = "Виктор Васнецов",
+            year = 1898
+        ),
+        Photo(
+            id = R.drawable.ninth_shaft,
+            title = "Девятый вал",
+            author = "Иван Айвазовский",
+            year = 1850
+        ),
+        Photo(
+            id = R.drawable.trinity,
+            title = "Троица",
+            author = "Андрей Рублев",
+            year = 1425
+        ),
+    )
+}
